f3853537c75295e43395d87c55476b59d161bfab
Nicolas <73864341+FISHARMNIC@users.noreply.github.com>
2023-05-20 21:06:43 -0700

    Delete ham lang directory

--- ham lang/run/run-input.sh
+++
@@ -1,8 +0,0 @@
-# runs javascipt that waits for "ADDR" in memory
-# then runs qemu
-# then passes data into address specified
-
-RAM_ADDR=/Users/squijano/Documents/ham\ lang/asm/qemu-memory/qemu-ram
-echo -e " ------- \n\n \e[33m  Ctrl + C to exit program when done \e[0m \n\n -------" 
-node input.js "$RAM_ADDR" "hello world" &
-./run.sh
